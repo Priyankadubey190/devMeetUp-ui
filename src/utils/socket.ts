ea1d849db2d775a57b2e66d47e4e2af4340aca85
Priyankadubey190 <priyankadubey0811@gmail.com>
2026-05-24 16:10:38 +0530

    fix-socket-connection-for-production

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -2,11 +2,7 @@ import { io, type Socket } from "socket.io-client";
 import { BASE_URL } from "./constants";
 
 export const createSocketConnection = (): Socket => {
-  if (location.hostname === "localhost") {
-    return io(BASE_URL);
-  } else {
-    return io("/", {
-      path: "/api/socket.io",
-    });
-  }
+  return io(BASE_URL, {
+    withCredentials: true,
+  });
 };
