498e19e9eb641246810cfbea4057b488f5c9104b
Priyankadubey190 <priyankadubey0811@gmail.com>
2026-05-24 13:39:33 +0530

    backend deployed url added

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,5 @@
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:8080" : "/api";
+  typeof window !== "undefined" &&
+  (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? "http://localhost:8080"
+    : "https://devmeetupapp-api.onrender.com";
